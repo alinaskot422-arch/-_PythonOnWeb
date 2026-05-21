de3cc303cbfd267b510502cfe16e0c568a5378e2
KotlyAlina <alinaskot422@gmail.com>
2026-05-21 18:09:55 +1000

    calcul.py

--- калькулятор/calcul.py
+++ калькулятор/calcul.py
@@ -1,3 +1,4 @@
+# 1
 def calc():
     print("Калькулятор: +, -, *, /")
     a = int(input("Число 1: "))
@@ -13,7 +14,29 @@ def calc():
     elif op == '/':
         print(a / b)
     else:
-        print("Неверный оператор!")
+        print("Неверный оператор")
+
+calc()
+
+# 2
+def calc():
+    print("Калькулятор: +, -, *, /")
+    a = int(input("Число 1: "))
+    op = input("Оператор: ")
+    b = int(input("Число 2: "))
+
+    operations = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y if y != 0 else "Ошибка: деление на ноль"
+    }
+
+    if op in operations:
+        result = operations[op](a, b)
+        print(f"Результат: {result}")
+    else:
+        print("Неверный оператор")
 
 calc()
 
