9985a5049eb2cbbc3c39cb5d922e208ca0633aaa
KotlyAlina <alinaskot422@gmail.com>
2026-05-21 17:33:34 +1000

    Add files via upload

---
+++ zadania_1.py
@@ -0,0 +1,34 @@
+# 1
+price1 = 150
+price2 = 250
+result = price1 + price2
+print("Задача 1:", result) 
+
+# 2
+length = 10
+width = 5
+area = length * width
+print("Задача 2:", area) 
+
+# 3
+a = [5, 4, 5]
+b = sum(a) / len(a)
+print("Задача 3:", round(b, 2))  
+
+# 4
+seconds = 135
+minutes = seconds // 60
+r_seconds = seconds % 60
+print(f"Задача 4: {seconds} секунд -> {minutes} минут и {r_seconds} секунд")
+
+# 5
+candies = 20
+friends = 3
+each = candies // friends
+o = candies % friends
+print(f"Задача 5: Каждому по {each}, останется {o}")
+
+# 6
+square_7 = 7 ** 2
+cube_3 = 3 ** 3
+print(f"Задача 6: 7**2 = {square_7}, 3**3 = {cube_3}")
